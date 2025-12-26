0e79c63be4cfba04516abb31f08f7ff354e1b18b
StealingYourCode <156914474+Emii-Chann@users.noreply.github.com>
2025-12-26 11:42:19 +0100

    init backend project

---
+++ backend/src/test/java/com/nightout_reserve/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nightout_reserve.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
