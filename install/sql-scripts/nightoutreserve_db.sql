99c54b9947374b5eb669206e0a23c5e24a8d2a4e
Andoorr <157022753+Andoorr@users.noreply.github.com>
2026-03-01 13:07:35 +0100

    SQL frissités

--- install/sql-scripts/nightoutreserve_db.sql
+++ install/sql-scripts/nightoutreserve_db.sql
@@ -1,27 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2026. Feb 03. 19:59
--- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.2.12
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+DROP DATABASE IF EXISTS NightoutReserve_DB;
+
+CREATE DATABASE NightoutReserve_DB
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE NightoutReserve_DB;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Adatbázis: `nightoutreserve_db`
---
-
--- --------------------------------------------------------
+-- ------------------------------------------------------------
 
 --
 -- Tábla szerkezet ehhez a táblához `asztalok`
