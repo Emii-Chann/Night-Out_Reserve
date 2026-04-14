6173d3ee9c09fc589902c92f97376afb93dad2e6
Andoorr <157022753+Andoorr@users.noreply.github.com>
2026-04-14 22:53:32 +0200

    Update BackendApplication.java

--- backend/src/main/java/com/nightout_reserve/backend/BackendApplication.java
+++ backend/src/main/java/com/nightout_reserve/backend/BackendApplication.java
@@ -2,17 +2,16 @@ package com.nightout_reserve.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.nightout_reserve.backend"})
+@ComponentScan(basePackages = {"com.nightout_reserve.backend", "com.nightout.backend"}) 
+@EnableJpaRepositories(basePackages = {"com.nightout_reserve.backend", "com.nightout.backend"})
+@EntityScan(basePackages = {"com.nightout_reserve.backend", "com.nightout.backend"})
 public class BackendApplication {
-    
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
