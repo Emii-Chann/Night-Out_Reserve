a37c171facffe55395d195d8614c2404c21d53fa
Andoorr <157022753+Andoorr@users.noreply.github.com>
2026-04-15 20:23:56 +0200

    Update BackendApplication.java

--- backend/src/main/java/com/nightout_reserve/backend/BackendApplication.java
+++ backend/src/main/java/com/nightout_reserve/backend/BackendApplication.java
@@ -2,14 +2,11 @@ package com.nightout_reserve.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.nightout_reserve.backend", "com.nightout.backend"}) 
-@EnableJpaRepositories(basePackages = {"com.nightout_reserve.backend", "com.nightout.backend"})
-@EntityScan(basePackages = {"com.nightout_reserve.backend", "com.nightout.backend"})
+// Kényszerítjük, hogy ebben a csomagban mindent (configot, controllert) keressen meg
+@ComponentScan(basePackages = "com.nightout_reserve.backend") 
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
