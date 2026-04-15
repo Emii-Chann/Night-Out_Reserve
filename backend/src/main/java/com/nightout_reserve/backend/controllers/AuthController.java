c824117c7731a223eea16c789a381aa659eecb99
Andoorr <157022753+Andoorr@users.noreply.github.com>
2026-04-15 20:42:50 +0200

    Update AuthController.java

--- backend/src/main/java/com/nightout_reserve/backend/controllers/AuthController.java
+++ backend/src/main/java/com/nightout_reserve/backend/controllers/AuthController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nightout_reserve.backend.dto.UserLoginDTO;
-import com.nightout_reserve.backend.models.PasswordResetRequest;
 import com.nightout_reserve.backend.models.PasswordResetToken;
 import com.nightout_reserve.backend.models.User;
 import com.nightout_reserve.backend.repositories.PasswordResetTokenRepository;
@@ -51,26 +50,6 @@ public class AuthController {
     }
 
 
-    @PostMapping("/reset-password")
-    public ResponseEntity<?> resetPassword(@RequestBody PasswordResetRequest request) {
-        
-        // 1. Megkeressük az embert az email alapján
-        User user = userRepository.findByEmail(request.getEmail());        
-        if (user == null) {
-            // Ha nincs ilyen email az adatbázisban
-            return ResponseEntity.status(404).body("Nem található felhasználó ezzel az email címmel!");
-        }
-        
-        // 2. Beállítjuk az új jelszót
-        // ⚠️ FONTOS: Ha használsz jelszó titkosítást (BCryptPasswordEncoder), akkor itt titkosítva mentsd el!
-        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
-    
-        
-        // 3. Elmentjük az adatbázisba
-        userRepository.save(user);
-        
-        return ResponseEntity.ok("Sikeres jelszóváltoztatás!");
-    }
 
 
 
