6b7834f32f5359ae91a4e4ed87f801b5e923759a
Andoorr <157022753+Andoorr@users.noreply.github.com>
2026-04-19 14:54:18 +0200

    Update admin-dashboard.js

--- frontend/app/scripts/admin-dashboard.js
+++ frontend/app/scripts/admin-dashboard.js
@@ -1,8 +1,5 @@
+
 // --- 1. ADATOK LEKÉRÉSE ÉS SZŰRÉSE ---
-const adminId = localStorage.getItem("nr_admin_id");
-    if (!adminId) {
-        window.location.href = "./admin-login.html";
-    }
 
 
 async function getReservations() {
@@ -520,6 +517,8 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     } else {
         console.warn("Nincs bejelentkezett tulajdonos a localStorage-ben!");
+        window.location.href = "./admin-login.html";
+
     }
 });
 
